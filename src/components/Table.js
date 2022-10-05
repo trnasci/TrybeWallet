bbf2c26ec4a27e964b9793c9d1c22f70d0c5bd4e
Thiago Reis Nascimento <trnasci@gmail.com>
2022-10-05 10:56:13 -0300

    Ajustando para o github

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
     return (
-      <div>Table2</div>
+      <table>
+        <tr>
+          <th scope="col">Descrição</th>
+          <th scope="col">Tag</th>
+          <th scope="col">Método de pagamento</th>
+          <th scope="col">valor</th>
+          <th scope="col">Moeda</th>
+          <th scope="col">Câmbio utilizado</th>
+          <th scope="col">Valor convertido</th>
+          <th scope="col">Moeda de conversão</th>
+          <th scope="col">Editar/Excluir</th>
+        </tr>
+      </table>
     );
   }
 }
 
-export default Table;
+function mapStateToProps(state) {
+  return { ...state.wallet };
+}
+
+export default connect(mapStateToProps)(Table);
